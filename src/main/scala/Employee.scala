bba347025b08378285be752fa4bc05f460c27424
luyang <xieoy@hotmail.com>
2019-01-03 19:26:01 +0800

    minor

--- src/main/scala/Employee.scala
+++ src/main/scala/Employee.scala
@@ -1,6 +1,6 @@
 import scalafx.beans.property.StringProperty
 
 class Employee(_name: String, _email: String) {
-  val name: StringProperty = new StringProperty(_name)
-  val email: StringProperty = new StringProperty(_email)
+  val name = new StringProperty(_name)
+  val email = new StringProperty(_email)
 }
